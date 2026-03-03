a1698a2edcbff40b7a6d050d729ea6ae51b3302d
AlanGaBer11 <alangaber11@gmail.com>
2026-03-02 21:27:32 -0600

    feat: implement findByName method in ICategoryRepository interface

--- backend/src/shared/interfaces/categoryRepository.interface.js
+++ backend/src/shared/interfaces/categoryRepository.interface.js
@@ -1,3 +1,8 @@
 import IBaseRepository from "./baseRepository.interface.js";
-class ICategoryRepository extends IBaseRepository {}
+class ICategoryRepository extends IBaseRepository {
+  // Método para buscar una categoría por su nombre
+  async findByName(name) {
+    throw new Error("Método no implementado.");
+  }
+}
 export default ICategoryRepository;
