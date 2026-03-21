aa9d5b49629905cdcc1fd8449c0d52a4f7beeeaa
AlanGaBer11 <alangaber11@gmail.com>
2026-03-20 23:21:57 -0600

    feat: Add new posts module with its model, service, process, controller, and API routes.

---
+++ backend/src/modules/posts/routes/post.routes.js
@@ -0,0 +1,37 @@
+import PostController from "../controller/post.controller.js";
+import { Router } from "express";
+
+class PostRoutes {
+  constructor(postController) {
+    /**
+     * @param {import('../controller/post.controller.js').default} postController
+     */
+
+    this.router = Router();
+
+    /**
+     * @type {import('../controller/post.controller.js').default}
+     */
+    this.postController = postController;
+
+    // Configurar las rutas de posts
+    this.configureRoutes();
+  }
+
+  // Método estático para crear una instancia de las rutas con el controlador inyectado
+  static async create() {
+    const controller = await PostController.create();
+    return new PostRoutes(controller);
+  }
+
+  // Método para configurar las rutas de posts
+  configureRoutes() {
+    // Ruta para obtener todos los posts
+    this.router.get(
+      "",
+      this.postController.findAllPosts.bind(this.postController),
+    );
+  }
+}
+
+export default PostRoutes;
