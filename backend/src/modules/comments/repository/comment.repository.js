5642070997be17088f3e1aba0dff395ca65dcf7f
Alan García <alangaber11@gmail.com>
2026-05-09 23:07:55 -0600

    feat: enhance findAll method in CommentRepository to filter by post_id and user_id

--- backend/src/modules/comments/repository/comment.repository.js
+++ backend/src/modules/comments/repository/comment.repository.js
@@ -4,8 +4,10 @@ import Comment from "../model/Comment.js";
 class CommentRepository extends ICommentRepository {
   // Método para buscar todos los comentarios
   async findAll(page = 1, limit = 10, query = {}) {
-    const { status } = query;
+    const { post_id, user_id, status } = query;
     const whereClause = {};
+    if (post_id) whereClause.post_id = post_id;
+    if (user_id) whereClause.user_id = user_id;
     if (status) whereClause.status = status;
 
     const offset = (page - 1) * limit;
