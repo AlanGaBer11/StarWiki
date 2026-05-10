d643b499c84e9d49733c94e43a7ee36fa9f6e515
Alan García <alangaber11@gmail.com>
2026-05-09 22:21:58 -0600

    feat: implement CommentController, CommentProcess, CommentService, and CommentRepository for managing comments

---
+++ backend/src/modules/comments/repository/comment.repository.js
@@ -0,0 +1,28 @@
+import ICommentRepository from "#shared/interfaces/commentRepository.interface.js";
+import Comment from "../model/Comment.js";
+
+class CommentRepository extends ICommentRepository {
+  // Método para buscar todos los comentarios
+  async findAll(page = 1, limit = 10, query = {}) {
+    const { status } = query;
+    const whereClause = {};
+    if (status) whereClause.status = status;
+
+    const offset = (page - 1) * limit;
+
+    const { count, rows } = await Comment.findAndCountAll({
+      where: whereClause,
+      offset,
+      limit,
+      order: [["created_at", "DESC"]],
+    });
+    return {
+      comments: rows,
+      totalComments: count,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+    };
+  }
+}
+
+export default CommentRepository;
---
+++ backend/src/modules/comments/services/comment.service.js
@@ -0,0 +1,52 @@
+import RepositoryConfig from "#config/repository.js";
+import logger from "#config/chalk.js";
+
+/* DTOs */
+
+/* Errores */
+
+class CommentService {
+  /**
+   * @param {import("../repository/comment.repository.js").default} commentRepository
+   */
+  constructor(commentRepository) {
+    this.commentRepository = commentRepository;
+  }
+
+  // Método estático para crear una instancia del servicio con el repositorio adecuado
+  static async create() {
+    const repo = await RepositoryConfig.getRepository("comment");
+    return new CommentService(repo);
+  }
+
+  // Método para obtener todos los comentarios
+  async findAllComments(page, limit, query) {
+    try {
+      const result = await this.commentRepository.findAll(page, limit, query);
+
+      // Si no se encontraron comentarios, retornar un objeto con valores predeterminados
+      if (!result || result.comments.length === 0) {
+        return {
+          comments: [],
+          totalComments: 0,
+          totalPages: 0,
+          currentPage: 0,
+        };
+      }
+
+      // Registrar la cantidad de comentarios encontrados
+      logger.info(`Se encontraron ${result.comments.length} comentarios.`);
+      return {
+        comments: result.comments,
+        totalComments: result.totalComments,
+        totalPages: result.totalPages,
+        currentPage: result.currentPage,
+      };
+    } catch (error) {
+      logger.error("Error al buscar comentarios:", error.message);
+      throw error;
+    }
+  }
+}
+
+export default CommentService;
